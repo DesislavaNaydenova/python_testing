936de5250e8668e34d4d83e4a93e6a55ebfffdd6
Desislava Naydenova <deskata.01@gmail.com>
2024-07-08 10:42:48 +0000

    Test the input of the function if it's empty, if there are 2 evens if there is uneven number of content

--- evens.py
+++ evens.py
@@ -9,7 +9,15 @@ def even_number_of_evens(numbers):
     """
     
     if isinstance(numbers, list):
-        return True
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+
+        for n in numbers:
+            if n % 2 == 0:
+                evens +=1
+        return evens % 2 == 0
     else:
         raise TypeError("A list was not passed into the function")
 
