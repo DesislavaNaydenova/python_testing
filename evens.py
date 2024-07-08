ed60e82cb3951b230ed2be7a50bb285da53b9955
Desislava Naydenova <deskata.01@gmail.com>
2024-07-08 10:31:22 +0000

    Add testif the function throws error when no list is passed, run the test and write a function to throw error, when no list is passed

--- evens.py
+++ evens.py
@@ -8,7 +8,10 @@ def even_number_of_evens(numbers):
     if the number if even numbers is even, return True
     """
     
-    return None
+    if isinstance(numbers, list):
+        return True
+    else:
+        raise TypeError("A list was not passed into the function")
 
 if __name__ =='__main__':
     print(even_number_of_evens(5))
